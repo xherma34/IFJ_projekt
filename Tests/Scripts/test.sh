26f6fec652d7121d237dd87d25c545fab4b83b4d
hoza2000 <zdenek.honza@email.cz>
2021-12-08 14:03:21 -0600

    script update

--- Tests/Scripts/test.sh
+++ Tests/Scripts/test.sh
@@ -9,6 +9,7 @@ for((i=1;i<$numberOfFiles+1;i++))
 do
   	echo "---Test" $i "---";
   	echo
+	#valgrind --leak-check=full --track-origins=yes $PWD/ifj21  < $PWD/Tests/Inputs/testInput_$i.txt 2> $PWD/Tests/Outputs/testOutput_$i.txt
 	$PWD/ifj21  < $PWD/Tests/Inputs/testInput_$i.txt 2> $PWD/Tests/Outputs/testOutput_$i.txt
     diff -us  $PWD/Tests/Outputs/testOutput_$i.txt $PWD/Tests/Referals/referOutput_$i.txt
   	echo
